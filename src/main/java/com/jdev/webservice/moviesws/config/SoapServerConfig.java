8bca498687d0cb6d74abee5f3cf4954f3ab9219e
Max Wendel <maxwendelffaraujo@gmail.com>
2019-04-30 16:26:18 -0300

    Add Spring WS Validations

--- src/main/java/com/jdev/webservice/moviesws/config/SoapServerConfig.java
+++ src/main/java/com/jdev/webservice/moviesws/config/SoapServerConfig.java
@@ -9,18 +9,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.soap.server.endpoint.SoapFaultDefinition;
 import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.List;
 import java.util.Properties;
 
 @EnableWs
 @Configuration
-public class SoapServerConfig {
+public class SoapServerConfig extends WsConfigurerAdapter {
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Bean
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext appContext) {
@@ -59,5 +63,14 @@ public class SoapServerConfig {
         exceptionResolver.setExceptionMappings(errorMappings);
         exceptionResolver.setOrder(1);
         return exceptionResolver;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        PayloadValidatingInterceptor validatingInterceptor = new PayloadValidatingInterceptor();
+        validatingInterceptor.setValidateRequest(true);
+        validatingInterceptor.setValidateResponse(true);
+        validatingInterceptor.setXsdSchema(moviesSchema());
+        interceptors.add(validatingInterceptor);
     }
 }
