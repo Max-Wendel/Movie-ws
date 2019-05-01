b23b0a00fbecfbd35b28e31933d70b2fdc554ffc
Max Wendel <maxwendelffaraujo@alu.ufc.br>
2019-05-01 14:51:51 -0300

    Remove Exceptions

--- src/main/java/com/jdev/webservice/moviesws/config/SoapServerConfig.java
+++ src/main/java/com/jdev/webservice/moviesws/config/SoapServerConfig.java
@@ -1,8 +1,6 @@
 package com.jdev.webservice.moviesws.config;
 
 import com.jdev.webservice.moviesws.endpoint.MovieEndpoint;
-import com.jdev.webservice.moviesws.exception.DetailSoapFaultDefinitionExceptionResolver;
-import com.jdev.webservice.moviesws.exception.ServiceFaultException;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -47,22 +45,6 @@ public class SoapServerConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema moviesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("/xsd/movies.xsd"));
-    }
-
-    @Bean
-    public SoapFaultMappingExceptionResolver exceptionResolver() {
-        SoapFaultMappingExceptionResolver exceptionResolver = new DetailSoapFaultDefinitionExceptionResolver();
-
-        SoapFaultDefinition faultDefinition = new SoapFaultDefinition();
-        faultDefinition.setFaultCode(SoapFaultDefinition.SERVER);
-        exceptionResolver.setDefaultFault(faultDefinition);
-
-        Properties errorMappings = new Properties();
-        errorMappings.setProperty(Exception.class.getName(), SoapFaultDefinition.SERVER.toString());
-        errorMappings.setProperty(ServiceFaultException.class.getName(), SoapFaultDefinition.SERVER.toString());
-        exceptionResolver.setExceptionMappings(errorMappings);
-        exceptionResolver.setOrder(1);
-        return exceptionResolver;
     }
 
     @Override
